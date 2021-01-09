2e69a7057717d81d16318a54815a86e259835700
Ugoo <54181133+Bondesvick@users.noreply.github.com>
2021-01-09 02:42:54 +0100

    Added domian models

---
+++ CurrencyAPI/Models/Funding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletSystemAPI.Models
+{
+    public class Funding
+    {
+        public int Id { get; set; }
+
+        public string CurrencyId { get; set; }
+
+        [ForeignKey("CurrencyId")]
+        public Currency Currency { get; set; }
+    }
+}
---
+++ CurrencyAPI/Models/Wallet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletSystemAPI.Models
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public Decimal Balance { get; set; }
+
+        public string CurrencyId { get; set; }
+
+        [ForeignKey("CurrencyId")]
+        public Currency Currency { get; set; }
+
+        public string OwnerId { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public User Owner { get; set; }
+    }
+}
