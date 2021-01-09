6367c9fee522a00c211576a96e8814025c88ff0b
Ugoo <54181133+Bondesvick@users.noreply.github.com>
2021-01-10 00:05:20 +0100

    Added models and methods for fetching from currency rate API

---
+++ CurrencyAPI/Dtos/RequestRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace WalletSystemAPI.Dtos
+{
+    public class RequestRoot
+    {
+        public bool Success { get; set; }
+        public int Timestamp { get; set; }
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public Rates Rates { get; set; }
+    }
+}
---
+++ CurrencyAPI/Helpers/CurrencyRate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WalletSystemAPI.Dtos;
+
+namespace WalletSystemAPI.Helpers
+{
+    public static class CurrencyRate
+    {
+        public static async Task<RequestRoot> GetExchangeRate()
+        {
+            using HttpClient client = new HttpClient();
+
+            using HttpResponseMessage response = await client.GetAsync("http://data.fixer.io/api/latest?access_key=c9d6219ffc703c54084500873bf7b73e&symbols");
+
+            if (response.IsSuccessStatusCode)
+            {
+                using HttpContent content = response.Content;
+                var myContent = await content.ReadAsStringAsync();
+
+                var value = JsonConvert.DeserializeObject<RequestRoot>(myContent);
+
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
